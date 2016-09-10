f84eed7eb00944a59616e4efc698e81be233676c
Fábio Pereira de Souza <fabiosouza.web@gmail.com>
2016-09-10 16:07:39 -0300

    Angular2 Docs Quickstart and Hero apps

---
+++ angular2-tour-of-heroes/app/hero-detail.component.ts
@@ -0,0 +1,41 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+@Component({
+	selector: 'my-hero-detail',
+	templateUrl: 'app/hero-detail.component.html',
+	styleUrls: ['app/hero-detail.component.css']
+})
+
+export class HeroDetailComponent implements OnInit {
+	
+	@Input()
+	hero: Hero;
+
+	constructor(
+		private heroService: HeroService,
+		private route: ActivatedRoute) {
+	}
+
+	ngOnInit(): void {
+		this.route.params.forEach((params: Params) => {
+	    let id = +params['id']; // "+" convert number id to string!
+	    this.heroService.getHero(id)
+	    .then(hero => this.hero = hero);
+	});
+	}
+
+	save(): void {
+		this.heroService.update(this.hero)
+		.then(this.goBack);
+	}
+
+
+	goBack(): void {
+		window.history.back();
+	}
+}
+
